f1010e41dfe1d11f549ccaefc85952726a4a4c30
guo-yu <o.u.turing@gmail.com>
2013-11-21 20:48:23 +0800

    0.0.1: bug fixed

--- libs/cli.js
+++ libs/cli.js
@@ -4,6 +4,7 @@ var List = require('term-list'),
     _ = require('underscore'),
     consoler = require('consoler'),
     colors = require('colors'),
+    moment = require('moment'),
     sdk = require('./sdk');
 
 var wash = function(k, v) {
@@ -11,11 +12,11 @@ var wash = function(k, v) {
         var last = _.last(v);
         return {
             timestamp: last[0],
-            last: 'RMB: ' + last[1]
+            last: '¥ ' + last[1]
         }
     } else if (k == 'bitstamp') {
         var data = v;
-        data.last = '$: ' + data.last;
+        data.last = '$ ' + data.last;
         return data;
     } else {
         return v;
@@ -23,10 +24,37 @@ var wash = function(k, v) {
 };
 
 var label = function(key, data) {
-    var str = key + ' ';
+    var str = ' ';
     if (data.last) str += colors.green(data.last) + ' ';
-    if (data.timestamp) str += colors.grey(new Date(data.timestamp * 1000).getHours() + ':' + new Date(data.timestamp * 1000).getMinutes()) + ' ';
+    if (data.timestamp) {
+        if (key == 'btcchina') {
+            str += colors.grey(moment(data.timestamp).format('HH:mm')) + ' '
+        } else {
+            str += colors.grey(moment.unix(data.timestamp).format('HH:mm')) + ' '
+        }
+    }
     return str;
+}
+
+var update = function(menu, within) {
+    _.each(menu.exchangers, function(item, index){
+        within(item, index);
+        bitcoin.price(item.name, function(err, result){
+            if (!err) {
+                var data = wash(item.name, result);
+                menu._update(index, label(item.name, data));
+            } else {
+                menu._update(index, colors.red('request fail' + err.toString()));
+            }
+        });
+    });
+}
+
+var init = function(menu) {
+    update(menu, function(item){
+        menu.add(item.url, item.name + colors.yellow(' loading...'));
+    });
+    menu.start();
 }
 
 module.exports = function() {
@@ -37,25 +65,17 @@ module.exports = function() {
     menu._update = function(index, label) {
         this.at(index).label = this.exchangers[index].name + ' ' + label;
         this.draw();
-    }
+    };
     menu.exchangers = bitcoin.exchangers(sdk);
-    consoler.loading('loading bitcoin prices...');
-    _.each(menu.exchangers, function(item, index){
-        menu.add(item.url, item.name + colors.yellow(' loading...'));
-        bitcoin.price(item.name, function(err, result){
-            if (!err) {
-                var data = wash(item.name, result);
-                menu._update(index, label(item.name, data));
-            } else {
-                menu._update(index, colors.red('request fail'));
-            }
-        });
-    });
-    menu.start();
     menu.on('keypress', function(key, item) {
-        console.log(item.url);
+        if (key.name == 'enter') {
+            update(menu, function(item, index){
+                menu._update(index, colors.yellow('updating...'))
+            });
+        }
     });
     menu.on('empty', function() {
         menu.stop();
     });
+    init(menu);
 }
