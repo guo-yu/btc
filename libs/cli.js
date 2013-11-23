2d14a82e6ed947d7d5741dc289b8da1fed4c3d2b
guo-yu <o.u.turing@gmail.com>
2013-11-23 21:27:53 +0800

    0.0.3: bug fixed
    fixed a if-undefined bug

--- libs/cli.js
+++ libs/cli.js
@@ -20,28 +20,28 @@ var wash = function(k, v) {
         result.last = v.last;
     } else if (k == 'mtgox') {
         result.stat = v.data && v.data.last ? 'ok' : 'error';
-        result.last = v.data.last.value;
+        result.last = v.data && v.data.last ? v.data.last.value : null;
     } else if (k == 'fxbtc') {
         result.stat = v.result ? 'ok' : 'error';
-        result.last = v.ticker.ask;
+        result.last = v.ticker ? v.ticker.ask : null;
     } else if (k == 'okcoin') {
         result.stat = v.ticker ? 'ok' : 'error';
-        result.last = v.ticker.last;
+        result.last = v.ticker ? v.ticker.last : null;
     } else if (k == 'btctrade') {
         result.stat = v.last ? 'ok' : 'error';
         result.last = v.last;
     } else if (k == 'chbtc') {
         result.stat = v.ticker ? 'ok' : 'error';
-        result.last = v.ticker.last;
+        result.last = v.ticker ? v.ticker.last : null;
     } else if (k == 'futures796') {
         result.stat = v.ticker ? 'ok' : 'error';
-        result.last = v.ticker.last;
+        result.last = v.ticker ? v.ticker.last : null;
     } else if (k == 'btc100') {
         result.stat = v.ticker ? 'ok' : 'error';
-        result.last = v.ticker.last;
+        result.last = v.ticker ? v.ticker.last : null;
     } else if (k == 'btce') {
         result.stat = v.ticker && v.ticker.last ? 'ok' : 'error';
-        result.last = v.ticker.last;
+        result.last = v.ticker ? v.ticker.last : null;
     }
     return result;
 };
